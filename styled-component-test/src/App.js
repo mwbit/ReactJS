bf0f7ae5cf03ad15590afe3c58a13acef64a8a4a
mwbitt <mwbitt@gmail.com>
2018-11-06 14:06:55 -0200

    add projetct style-component-test

---
+++ styled-component-test/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Title, Wrapper } from './styles';
+
+class App extends Component {
+  render() {
+    return (
+      <Wrapper>
+        <Title>Project Title</Title>
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
