bd7eb4f4d335eca5410806402103e82579c351ea
mwbitt <mwbitt@gmail.com>
2018-11-06 14:14:40 -0200

    add button

--- styled-component-test/src/App.js
+++ styled-component-test/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { Title, Wrapper } from './styles';
+import { Title, Wrapper, Button } from './styles';
 
 class App extends Component {
   render() {
     return (
       <Wrapper>
         <Title>Project Title</Title>
+        <Button>Normal</Button>
+        <Button primary>Primary</Button>
       </Wrapper>
     );
   }
--- styled-component-test/src/styles.js
+++ styled-component-test/src/styles.js
@@ -11,4 +11,16 @@ const Wrapper = styled.section`
   background: papayawhip;
 `;
 
-export {Title, Wrapper}
+const Button = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: ${props => props.primary ? "palevioletred" : "white"};
+  color: ${props => props.primary ? "white" : "palevioletred"};
+  
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
+export {Title, Wrapper, Button}
