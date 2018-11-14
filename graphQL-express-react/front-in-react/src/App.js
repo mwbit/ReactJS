9bbcbbc2564e78b2033ed4164f5aeacd5da989dd
mwbitt <mwbitt@gmail.com>
2018-11-14 12:30:43 -0200

    add frontend in react

---
+++ graphQL-express-react/front-in-react/src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo'
+
+import BookList from './components/BookList';
+
+const client = new ApolloClient({
+  uri:"http://localhost:4000/graphql"
+})
+
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+      <div>
+        <h1>List of books </h1>
+        <BookList />
+      </div>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
---
+++ graphQL-express-react/front-in-react/src/components/BookList.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { gql } from 'apollo-boost'
+import {graphql} from 'react-apollo';
+
+const getBooksQuery = gql`
+    {
+        books {
+            name
+            id
+        }
+    }
+`
+
+class BookList extends Component {
+    displayBooksList(){
+        let data = this.props.data
+        if(data.loading){
+            return (<div>Loadingbooks...</div>)
+        }else{
+            return data.books.map(book =>{
+                return(
+                    <li key={book.id}>{book.name}</li>
+                )
+            }) 
+        }
+    }
+    render() {
+       
+        return (
+            <div>
+                <ul id="book-list">
+                   {this.displayBooksList()}
+                </ul>
+            </div>
+
+        );
+    }
+}
+
+export default graphql(getBooksQuery)(BookList);
