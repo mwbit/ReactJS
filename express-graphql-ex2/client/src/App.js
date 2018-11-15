594f78bbce41c189ff8b46fa293396c00c7b1cf4
mwbitt <mwbitt@gmail.com>
2018-11-15 17:35:18 -0200

    add server cors and client

---
+++ express-graphql-ex2/client/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo'
+import Launches from './components/Launches'
+
+import './App.css';
+import logo from './logo.png'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql'
+})
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client = { client }>
+      <div className="App">
+        <img src={logo} alt="spaceX" style={{with:300, display:'block', margin: 'auto'}}/>
+        <Launches />
+      </div>
+      </ApolloProvider>
+    ); 
+  }
+}
+
+export default App;
--- express-graphql-ex2/server/server.js
+++ express-graphql-ex2/server/server.js
@@ -3,6 +3,10 @@ const graphqlHTTP = require('express-graphql')
 const app = express()
 const schema = require('./schema')
 const PORT = process.env.PORT || 5000
+const cors = require('cors')
+
+
+app.use(cors())
 
 app.use('/graphql', graphqlHTTP({
     schema,
