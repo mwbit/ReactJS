0e21ef29f60272e341db9dacb9e2a0caf4585133
mwbitt <mwbitt@gmail.com>
2018-11-15 14:55:13 -0200

    add schema launch

--- express-graphql-ex2/server/schema.js
+++ express-graphql-ex2/server/schema.js
@@ -12,13 +12,12 @@ const axios = require('axios')
 const LaunchType = new GraphQLObjectType({
     name: 'Launch',
     fields: () => ({
-        fligth_number: { type: GraphQLInt},
+        flight_number: { type: GraphQLInt},
         mission_name: { type: GraphQLString},
         launch_year: { type: GraphQLString},
         launch_date_local: { type: GraphQLString},
         launch_success: { type: GraphQLBoolean},
         rocket: { type: RocketType},
-
     })
 })
 
@@ -35,12 +34,24 @@ const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
         launches: {
-            type:new GraphQLList(LaunchType),
+            type: new GraphQLList(LaunchType),
             resolve(parent, args){
                 return axios.get('https://api.spacexdata.com/v3/launches')
                 .then(res => res.data)        
             }
+        },
+        launch: {
+            type: LaunchType,
+            args:{
+                flight_number: {type: GraphQLInt}
+            },
+            resolve(parent,args){
+                return axios.get(`https://api.spacexdata.com/v3/launches/${args.flight_number}}`)
+                .then(res => res.data)        
+            
+            }
         }
+
     }
 })
 
