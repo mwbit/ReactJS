f71cfd51647848729a2f96b3d40d321ddee506be
Marcelo <mwbitt@gmail.com>
2019-03-12 12:23:55 -0300

    include Components and test

--- reactjs-test-ex1/src/App.js
+++ reactjs-test-ex1/src/App.js
@@ -1,8 +1,37 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Home from "./Home";
+
+const fakeDatabase = {
+  Heisenberg: [
+    "8am - Chemistry classes at school",
+    "12:30pm - Meet Jesse for lunch",
+    "15pm - Meet Gus at the Pollos Hermanos",
+    "20pm - Dinner by the pool with Hank and Marie"
+  ],
+  GusFring: [
+    "8am - Meeting with the DEA",
+    "15pm - Meet Walter at the Pollos Hermanos"
+  ]
+};
 
 class App extends Component {
+  static propTypes = {
+    isLoggedIn: PropTypes.bool,
+    username: PropTypes.string
+  };
   render() {
-    return <div>Test with Jest/Enzyme</div>;
+    const { isLoggedIn, username } = this.props;
+
+    return (
+      <div className="App">
+        {isLoggedIn ? (
+          <Home username={username} tasks={fakeDatabase[username]} />
+        ) : (
+          <p>Hello, visitor. Sign in to continue.</p>
+        )}
+      </div>
+    );
   }
 }
 
